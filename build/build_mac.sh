192e57ae395abc2cd44d5050ce0aa68b113e7def
Samuel Senior <s.m.senior@soton.ac.uk>
2019-08-28 20:31:12 +0100

    Added pyinstaller spec file

--- build/build_mac.sh
+++ build/build_mac.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 
 rm -r ../bin/mac/*
-
-pyinstaller --noconsole --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' ../BellBoardExporter.py;
-
-#pyinstaller --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' BellBoardExporter.py
-
+pyinstaller --noconsole --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' BellBoardExporter.spec;
 cp -r dist/BellBoardExporter ../bin/mac/BellBoardExporter&
 cp -r dist/BellBoardExporter.app ../bin/mac/BellBoardExporter.app;
-
-rm -r BellBoardExporter.spec build ../__pycache__ dist
+rm -r build ../__pycache__ dist
