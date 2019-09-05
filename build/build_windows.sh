23b3eef829602c7b5fcadbd112ba8463690948e7
Samuel Senior <S.M.Senior@soton.ac.uk>
2019-09-05 20:33:25 +0100

    Updated build script to only delete the old exe and nothing else in the bin dir

--- build/build_windows.sh
+++ build/build_windows.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -r ../bin/windows/*;
+rm -r ../bin/windows/BellBoardExporter*;
 pyinstaller --noconsole --onefile BellBoardExporter.spec;
 cp dist/BellBoardExporter ../bin/windows/BellBoardExporter;
 rm -r build ../__pycache__ dist;
